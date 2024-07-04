f22df0e570bb326355bbdd5a4843569c86d4db70
junhobaik <junhobaik@gmail.com>
2024-07-04 18:14:38 +0900

    chore: tsup, build 오류 수정 및 개선

--- server/main/tsup.config.ts
+++ server/main/tsup.config.ts
@@ -6,14 +6,14 @@ export default defineConfig({
   minify: true,
   sourcemap: false,
   treeshake: true,
-  splitting: true,
+  splitting: false,
   bundle: true,
   dts: false,
   external: ['@aws-sdk/client-dynamodb', '@aws-sdk/client-s3', '@aws-sdk/lib-dynamodb', '@aws-sdk/util-dynamodb'],
-
+  noExternal: ['uuid'],
   entry: ['src/index.ts'],
   outDir: 'dist',
   format: ['esm'],
-  target: 'esnext',
+  target: 'node20',
   banner: { js: 'import { createRequire } from "module";const require = createRequire(import.meta.url);' },
 });
