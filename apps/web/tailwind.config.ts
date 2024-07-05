9b82cf1e58586cb2c531deca38b3d6e4eace89a8
junhobaik <junhobaik@gmail.com>
2024-07-05 10:35:05 +0900

    conf: tailwind

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -4,20 +4,12 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    '../../node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   darkMode: 'class',
-  theme: {
-    extend: {
-      colors: {
-        background: 'var(--background)',
-        foreground: 'var(--foreground)',
-      },
-    },
-  },
+  theme: {},
   plugins: [nextui()],
 };
 export default config;
