3eb08bfd86a8badfef677e1fb1d78e9a981931f0
junhobaik <junhobaik@gmail.com>
2024-07-05 18:37:06 +0900

    feat: react-query

--- apps/web/src/page/home/providers.tsx
+++ apps/web/src/page/home/providers.tsx
@@ -3,7 +3,35 @@
 import React from 'react';
 
 import { NextUIProvider } from '@nextui-org/react';
+import { QueryClient, QueryClientProvider, isServer } from '@tanstack/react-query';
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
+const makeQueryClient = () => {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+      },
+    },
+  });
+};
+
+const getQueryClient = () => {
+  if (isServer) {
+    return makeQueryClient();
+  } else {
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
+  }
+};
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <NextUIProvider className="h-full min-h-full">{children}</NextUIProvider>;
+  const queryClient = getQueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <NextUIProvider className="h-full min-h-full">{children}</NextUIProvider>
+    </QueryClientProvider>
+  );
 }
