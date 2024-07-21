edac6a543f0337bf0cd3798a87162f9f9a476609
junhobaik <junhobaik@gmail.com>
2024-07-21 19:25:11 +0900

    refact: variables

--- apps/web/src/configs/variables.ts
+++ apps/web/src/configs/variables.ts
@@ -1,2 +1,13 @@
-export const SITE_URL = 'http://localhost:3000';
-export const BASE_URL = 'http://127.0.0.1:4000/local';
+export const SITE_URL_PRD = 'https://baik.dev';
+export const SITE_URL_DEV = 'http://localhost:3000';
+export const SITE_URL = process.env.NODE_ENV === 'production' ? SITE_URL_PRD : SITE_URL_DEV;
+
+export const BASE_URL_PRD = '';
+export const BASE_URL_DEV = 'http://127.0.0.1:4000/local';
+export const BASE_URL = process.env.NODE_ENV === 'production' ? BASE_URL_PRD : BASE_URL_DEV;
+
+export const SITE_TITLE = `Baik's dev archive`;
+export const SITE_TITLE_SUFFIX = ' | Baik';
+export const SITE_DESCRIPTION = '';
+
+export const MY_NAME = 'Junho Baik';
