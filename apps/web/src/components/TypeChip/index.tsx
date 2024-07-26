5054b82d33e6b48ec65d2384599ff5d766b282aa
junhobaik <junhobaik@gmail.com>
2024-07-26 23:06:00 +0900

    feat: TypeChip comp

---
+++ apps/web/src/components/TypeChip/index.tsx
@@ -0,0 +1,37 @@
+import { memo } from 'react';
+
+import { ArticleType } from '@baik/types';
+import clsx from 'clsx';
+
+const TypeChip = ({
+  type,
+  disabled = false,
+  onClick,
+  className = '',
+}: {
+  type: ArticleType;
+  disabled?: boolean;
+  onClick?: () => unknown;
+  className?: string;
+}) => {
+  return (
+    <div
+      className={clsx([
+        'inline-flex px-2 py-[2px] text-xs rounded-lg select-none',
+        type === 'post' && 'bg-blue-500 text-white',
+        type === 'shorts' && 'bg-rose-400 text-white',
+        type === 'clip' && 'bg-green-500 text-white',
+        disabled && '!bg-gray-200 !text-gray-500',
+        !!onClick && 'cursor-pointer',
+        className,
+      ])}
+      onClick={onClick}
+    >
+      {type === 'post' && 'Post'}
+      {type === 'shorts' && 'Shorts'}
+      {type === 'clip' && 'Clip'}
+    </div>
+  );
+};
+
+export default memo(TypeChip);
