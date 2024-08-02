7edee9fa2d227d41c70c6effd0449024217073ec
junhobaik <junhobaik@gmail.com>
2024-08-02 18:09:47 +0900

    refact: Article page

--- apps/web/src/page/archive/[pathname]/Screen.tsx
+++ apps/web/src/page/archive/[pathname]/Screen.tsx
@@ -3,6 +3,7 @@
 import React, { useEffect } from 'react';
 
 import { Article } from '@baik/types';
+import dayjs from 'dayjs';
 import { useAtom } from 'jotai';
 
 import MDContent from '@/components/MDContent';
@@ -22,8 +23,17 @@ const ArticleScreen = (props: ArticleScreenProps) => {
 
   return (
     <div>
-      <div className="flex flex-col max-w-[720px] mx-auto py-4">
-        <p className="text-3xl">{article.title}</p>
+      <div className="flex flex-col max-w-[720px] mx-auto py-4 pb-24">
+        {!!article.thumbnail_img_url && (
+          <img
+            src={article.thumbnail_img_url}
+            alt=""
+            className="w-full aspect-[1.91/1] object-cover rounded-2xl mb-6"
+          />
+        )}
+        <p className="text-3xl font-semibold text-gray-800 mb-4">{article.title}</p>
+        <p className="mb-12 text-gray-500">{dayjs(article.updated_date).format('YYYY.MM.DD')}</p>
+
         <MDContent content={article.content} codeBlockType="sandpack" />
       </div>
     </div>
