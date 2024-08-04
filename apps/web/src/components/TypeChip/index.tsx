38714a93a5761793af73298e5e8522f15353b27f
junhobaik <junhobaik@gmail.com>
2024-08-05 02:53:10 +0900

    refact: shorts 색상 변경

--- apps/web/src/components/TypeChip/index.tsx
+++ apps/web/src/components/TypeChip/index.tsx
@@ -19,7 +19,7 @@ const TypeChip = ({
       className={clsx([
         'inline-flex px-2 py-[2px] text-xs rounded-lg select-none',
         type === 'post' && 'bg-blue-500 text-white',
-        type === 'shorts' && 'bg-rose-400 text-white',
+        type === 'shorts' && 'bg-purple-400 text-white',
         type === 'clip' && 'bg-green-500 text-white',
         disabled && '!bg-gray-200 !text-gray-500',
         !!onClick && 'cursor-pointer',
