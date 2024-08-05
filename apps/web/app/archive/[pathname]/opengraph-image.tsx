d58ce32f67d12fa8b45df9ea6ab802606155ae94
junhobaik <junhobaik@gmail.com>
2024-08-05 18:22:09 +0900

    refact: openGraph 이미지 사이즈 다운

--- apps/web/app/archive/[pathname]/opengraph-image.tsx
+++ apps/web/app/archive/[pathname]/opengraph-image.tsx
@@ -6,9 +6,11 @@ export const runtime = 'edge';
 
 export const alt = "Article - Baik's archive";
 
+const divide = 4;
+
 export const size = {
-  width: 1200,
-  height: 630,
+  width: 1200 / divide,
+  height: 630 / divide,
 };
 
 export const contentType = 'image/png';
@@ -24,7 +26,7 @@ export default async function Image({ params }: { params: { pathname: string } }
       // ImageResponse JSX element
       <div
         style={{
-          fontSize: 64,
+          fontSize: 64 / divide,
           background: 'white',
           width: '100%',
           height: '100%',
@@ -33,7 +35,10 @@ export default async function Image({ params }: { params: { pathname: string } }
           alignItems: 'center',
           justifyContent: 'center',
           fontFamily: 'Pretendard',
-          padding: '80px 64px 0 64px',
+          paddingTop: 80 / divide,
+          paddingRight: 64 / divide,
+          paddingBottom: 0,
+          paddingLeft: 64 / divide,
         }}
       >
         <div
@@ -43,12 +48,12 @@ export default async function Image({ params }: { params: { pathname: string } }
             flexDirection: 'column',
             alignItems: 'center',
             justifyContent: 'center',
-            marginBottom: 200,
+            marginBottom: 200 / divide,
           }}
         >
           <div
             style={{
-              fontSize: 48,
+              fontSize: 48 / divide,
               fontWeight: 'bold',
               textAlign: 'center',
             }}
@@ -56,16 +61,16 @@ export default async function Image({ params }: { params: { pathname: string } }
             {title}
           </div>
 
-          <div style={{ display: 'flex', alignItems: 'center', marginTop: 40 }}>
+          <div style={{ display: 'flex', alignItems: 'center', marginTop: 40 / divide }}>
             <img
               src="https://d25sqaee97ji3k.cloudfront.net/0816bcfe-3f37-4982-90a8-e825ba5663a8.png"
               alt=""
-              width={54}
-              style={{ marginRight: 12 }}
+              width={54 / divide}
+              style={{ marginRight: 12 / divide }}
             />
             <div
               style={{
-                fontSize: 36,
+                fontSize: 36 / divide,
                 color: '#666',
               }}
             >
@@ -78,13 +83,13 @@ export default async function Image({ params }: { params: { pathname: string } }
           <img
             src={article.data.item.thumbnail_img_url}
             alt=""
-            width={1072}
+            width={1072 / divide}
             style={{
               position: 'absolute',
-              top: 430,
+              top: 430 / divide,
               objectFit: 'cover',
-              borderTopRightRadius: 64,
-              borderTopLeftRadius: 64,
+              borderTopRightRadius: 64 / divide,
+              borderTopLeftRadius: 64 / divide,
             }}
           />
         ) : null}
