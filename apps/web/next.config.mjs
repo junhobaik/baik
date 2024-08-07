aba54a50e1ee0259226d9e2793ebc20fe14e55a7
junhobaik <junhobaik@gmail.com>
2024-08-07 16:16:13 +0900

    conf: next

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -3,6 +3,9 @@ import { withSentryConfig } from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  compiler: {
+    styledComponents: true,
+  },
   experimental: {
     swcPlugins: [['@swc-jotai/react-refresh', {}]],
   },
@@ -15,6 +18,7 @@ const nextConfig = {
     ],
   },
 };
+
 export default withSentryConfig(nextConfig, {
   // For all available options, see:
   // https://github.com/getsentry/sentry-webpack-plugin#options
